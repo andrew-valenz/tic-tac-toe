2efcd99b40ebbbf0371115a3f8c1e3d6d4dcbe7d
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-26 11:31:05 -0800

    validated getting information back

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,23 +1,33 @@
-import React from 'react';
 import { createContext, useContext, useState } from 'react';
+import boardData from '../board-data.js';
 
 const GameContext = createContext();
-
 const GameProvider = ({ children }) => {
-    const [board, setBoard] = useState(boardData);
-    // const [current, setCurrentPlayer] = useState('');
-    // const [gameMessage] = useState(null);
-    // const [active] = useState(true/false);
-    return (
-        <GameContext.Provider value={{ board, setBoard }}>
-            {children}
-        </GameContext.Provider>
-    );
+  const [board, setBoard] = useState(boardData);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [gameMessage, setGameMessage] = useState('');
+  const [active, setActive] = useState(true);
+  return (
+    <GameContext.Provider
+      value={{
+        board,
+        setBoard,
+        currentPlayer,
+        setCurrentPlayer,
+        gameMessage,
+        setGameMessage,
+        active,
+        setActive,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
 };
 
 const useGameContext = () => {
-    const context = useContext(GameContext);
-    
-    return context;
-}
+  const context = useContext(GameContext);
+
+  return context;
+};
 export { useGameContext, GameProvider };
