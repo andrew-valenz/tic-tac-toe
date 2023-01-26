088566a8d941ea56b18078e22c7fc14b42d943a3
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-26 10:39:17 -0800

    set up GameContext

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { createContext, useContext, useState } from 'react';
 
 const GameContext = createContext();
-const GameProvider = ({ children }) => {
-    const [board, setBoard] = useState();
-    const [current, setCurrentPlayer] = useState('');
-    const [gameMessage] = useState(null);
-    const [active] = useState(true/false);
-}
 
-export { GameContext, GameProvider, useContext };
+const GameProvider = ({ children }) => {
+    const [board, setBoard] = useState(boardData);
+    // const [current, setCurrentPlayer] = useState('');
+    // const [gameMessage] = useState(null);
+    // const [active] = useState(true/false);
+    return (
+        <GameContext.Provider value={{ board, setBoard }}>
+            {children}
+        </GameContext.Provider>
+    );
+};
+
+const useGameContext = () => {
+    const context = useContext(GameContext);
+    
+    return context;
+}
+export { useGameContext, GameProvider };
