3acf0076415364e334c4d3ecc40cc34b9ad0022e
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-26 12:46:39 -0800

    added styling to present boxes in boardlike manner

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Box from './Box.js';
 import boardData from '../board-data.js';
+import './Board.css';
 
 export default function Board() {
   return (
-    <div>
+    <div className="board">
       {boardData.map((data) => (
         <Box key={data.space} />
       ))}
