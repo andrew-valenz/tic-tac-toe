71683462d27a00fbd3057f838fc481a92f001107
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-26 12:31:53 -0800

    all boxes rendering on page! Woohoo!

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Box from './Box.js';
+import boardData from '../board-data.js';
 
 export default function Board() {
-  return <Box />;
+  return (
+    <div>
+      {boardData.map((data) => (
+        <Box key={data.space} />
+      ))}
+    </div>
+  );
 }
